507892b9b906f57439a4df46e9fd9e86652c0b08
BlueHotDog <dannifriedland@gmail.com>
2012-03-21 18:23:31 +0200

    more work

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
     response.send('Hello World!');
 });
 
+app.get('/:user/:repo', function(req, res){
+    res.send('user ' + req.params.user + ' repo: ' + req.params.repo);
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log("Listening on " + port);
