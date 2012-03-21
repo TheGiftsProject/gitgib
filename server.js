9b37e2bda14dee5ce0dc5fa6a0acbda935c3f2a7
BlueHotDog <dannifriedland@gmail.com>
2012-03-21 18:11:26 +0200

    testing basic functionality

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-function server() {
+var express = require('express');
 
-}
+var app = express.createServer(express.logger());
 
-server.prototype.getScore = function(user, repo) {
+app.get('/', function(request, response) {
+    response.send('Hello World!');
+});
 
-}
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
